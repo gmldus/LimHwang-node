8203957747001a54dfa68c08f0b6412ccf70ea38
gmldus <gmldus0407@naver.com>
2020-01-21 21:09:59 +0900

    Express 프로젝트 생성

---
+++ app.js
@@ -0,0 +1,46 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+ }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
