fc6c49d3ac71cdc25efbeaf3524bde67ed51abe9
gmldus <gmldus0407@naver.com>
2020-01-23 00:27:05 +0900

    로그인

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -10,6 +10,28 @@ const getStudents = async(req, res) => {
     }
 }
 
+const doLogins = async(req, res) => {
+    try {
+        const body = req.body;
+        const login_id = body.login_id;
+        const login_password = body.login_password;
+        
+        const result = await Student.findOne({
+            where: {
+                id_students: login_id,
+                birth: login_password,
+            }
+        });
+        
+        if(result) res.status(200).json({ data: result });
+        else res.status(401).json({ errorMessage: '잘못된 회원정보입니다' });
+
+    } catch(error) {
+        res.status(401).json({ errorMessage: '로그인 실패' })
+    }
+}
+
 module.exports = { 
     getStudents,
+    doLogins,
 };
--- routes/students.js
+++ routes/students.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const studentController = require('../controllers/studentController');
 
 router.get('/get', studentController.getStudents);
+router.use('/login', studentController.doLogins);
 
 module.exports = router;
