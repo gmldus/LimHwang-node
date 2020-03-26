865b1d4522019a9be9ebeac8e21efcc17c4107cd
gmldus <gmldus0407@naver.com>
2020-03-26 11:35:27 +0900

    수강 학생목록

---
+++ controllers/teachController.js
@@ -0,0 +1,33 @@
+const Attendance = require('../models').Attendance;
+const sequelize = require('../models').sequelize;
+
+const getStudentList = async(req, res) => {
+    try {
+        const body = req.body;
+        const LectureID = body.LectureID;
+        
+        const studentList = await Attendance.findAll({
+
+            attributes: [sequelize.literal('DISTINCT `id_students`') ,'id_students'],
+
+            where: {
+                id_lectures: LectureID,
+            }
+        });
+        
+        if(studentList.length!==0){
+            res.status(200).json({ studentList: studentList });
+            console.log(studentList);
+        }
+          
+        else res.status(401).json({ errorMessage: '해당 정보가 존재하지 않습니다.' });
+
+    } catch(error) {
+        console.log(error);
+        res.status(401).json({ errorMessage: '학생들 정보 불러오기 실패' })
+    }
+}
+
+module.exports = { 
+    getStudentList,
+};
--- routes/teaches.js
+++ routes/teaches.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const teachController = require('../controllers/teachController');
+
+router.use('/getStudents', teachController.getStudentList);
+
 module.exports = router;
